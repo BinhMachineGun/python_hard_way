ecdac7e747295401e9b343a6199135fe8996a14e
BinhMachineGun <phanphuongbinh312@gmail.com>
2018-05-08 16:54:54 +0700

    Add ex10, ex10-fun of PHW.

---
+++ python-hard-way/ex10.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Author: PythonHardWay
+# @Date:   2018-05-08 16:20:54
+# @Last Modified by:   BinhPhan
+# @Last Modified time: 2018-05-08 16:21:10
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
