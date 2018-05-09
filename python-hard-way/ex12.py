b0689c65e3368629e9e4ac6f02e354fc63522652
BinhMachineGun <phanphuongbinh312@gmail.com>
2018-05-09 11:12:57 +0700

    Add ex12 of PHW.

---
+++ python-hard-way/ex12.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: PythonHardWay
+# @Date:   2018-05-09 11:05:41
+# @Last Modified by:   BinhPhan
+# @Last Modified time: 2018-05-09 11:06:32
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+print "So, you're %r old, %r tall and %r heavy." % (
+	age, height, weight)
