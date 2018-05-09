d0f1f8eb78acba8fe6359b185615ccee119deb23
BinhMachineGun <phanphuongbinh312@gmail.com>
2018-05-09 11:02:43 +0700

    Add ex11 and homework of PHW.

---
+++ python-hard-way/ex11-hw.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: BinhPhan
+# @Date:   2018-05-09 10:59:45
+# @Last Modified by:   BinhPhan
+# @Last Modified time: 2018-05-09 11:02:04
+
+print "What are you doing?",
+task = raw_input()
+print "How do you feel?",
+feel = raw_input()
+
+print "So, you're doing %r, you feel %r." % (
+	task, feel)
